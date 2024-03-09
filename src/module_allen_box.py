871266bd6166b59aa219dcfec418795a7ff54977
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-03-09 13:31:27 +0100

    improve allen box

--- src/module_allen_box.py
+++ src/module_allen_box.py
@@ -1,12 +1,48 @@
 # %%
 
+# Ensure python path is set to the root of the project
+import sys
+
 from build123d import *
 from yacv_server import *
 
+sys.path.append(os.path.join(os.path.abspath(''), 'src'))  # FIXME
+
+from core import grid, grid_dim
+from conn_grid import GridStack, GridScrewHeadHoles, GridScrewThreadHoles
+from global_params import wall
+
 # %% ================== MODELLING ==================
 
-with BuildPart(Plane.front) as part:
-    Box(1, 2, 3)
+box_width = max(grid_dim[0], 40 * MM)
+box_length = max(grid_dim[1], 35 * MM)
+box_height = 20 * MM
+
+with BuildPart() as conn_core:
+    with BuildPart(mode=Mode.PRIVATE) as grid_conn:
+        GridStack(parts=[GridScrewHeadHoles(repeat=grid, total_dimensions=grid_dim, rounded=False),
+                         GridScrewThreadHoles(repeat=grid, wrapped_screw_length=8 * MM,
+                                              # Minimal screw length
+                                              total_dimensions=grid_dim, rounded=False)])
+    add(grid_conn)
+    del grid_conn
+    RigidJoint(label="conn_core", joint_location=faces().group_by(Axis.Z)[0].face().center_location)
+
+with BuildPart() as box:
+    Box(box_width, box_length, box_height)
+    Box(box_width - 2 * wall, box_length - 2 * wall, box_height, mode=Mode.SUBTRACT)
+    Box(box_width - 2 * wall, box_length, box_height - 2 * wall, mode=Mode.SUBTRACT)
+    Box(box_width, box_length - 2 * wall, box_height - 2 * wall, mode=Mode.SUBTRACT)
+
+    loc = faces().group_by(Axis.Z)[-1].face().center_location
+    loc.orientation = Vector(loc.orientation.X + 180, loc.orientation.Y, loc.orientation.Z)
+    RigidJoint(label="top_conn_core", joint_location=loc)
+    del loc
+
+conn_core.joints["conn_core"].connect_to(box.joints["top_conn_core"])
+part = conn_core.part + box.part
+del conn_core, box
+show(part, 'conn_core')
 
 # %% ================== EXPORT ==================
 
@@ -17,4 +53,5 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     show_all()
     if os.getenv('CI', '') != '':
-        export_all(os.path.join(os.path.dirname(__file__), '..', 'export'))
+        export_all(os.path.join(os.path.dirname(__file__), '..', 'export'),
+                   export_filter=lambda name, obj: name == 'module_allen_box')
